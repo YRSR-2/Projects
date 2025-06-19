8bc9766b7593bfd6762104ce2ddb442b4b4fc0d8
YRSR-2 <yrvsa6514C@gmail.com>
2025-06-19 10:04:17 +0530

    Update booth_tb.v

--- Booths Multiplier/Radix 4/booth_tb.v
+++ Booths Multiplier/Radix 4/booth_tb.v
@@ -1,16 +1,16 @@
 module booth_tb();
 parameter N = 8;
 reg [N-1:0] data_inM,data_inQ;
-reg start,clk_100MHz;
+reg start,clk;
 wire [N+N-1:0] ans;
 
-booth #(.N(N),.alpha(3)) bt(start,clk_100MHz,data_inM,data_inQ,ans);
+booth #(.N(N),.alpha(3)) bt(start,clk,data_inM,data_inQ,ans);
 initial 
 begin
-start = 0;
-clk_100MHz = 1'b0;
+start=0;
+clk=1'b0;
 end
-always #5 clk_100MHz=~clk_100MHz;
+always #5 clk=~clk;
 initial 
 begin
 //#15 data_inM=8'b00011111;data_inQ=8'b00100111;
